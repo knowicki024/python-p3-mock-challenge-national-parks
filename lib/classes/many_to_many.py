de562f3eae1d9c54d48aa63ff51ada60667f4bbf
Katie N <146477745+knowicki024@users.noreply.github.com>
2024-01-15 18:38:13 -0500

    B&B

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,22 +1,43 @@
 class NationalPark:
 
+    all = []
+
     def __init__(self, name):
         self.name = name
+        self._trips = []
+        self._visitors = []
+
+        NationalPark.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter 
+    def name(self, name):
+        if isinstance(name, str) and not hasattr(self, "name") and len(name) >= 3:
+            self._name = name 
+        else:
+            raise Exception("no")
         
     def trips(self):
-        pass
+        return self._trips
     
     def visitors(self):
-        pass
+        return list(set(self._visitors))
     
     def total_visits(self):
-        pass
+        return len(self._trips)
     
     def best_visitor(self):
-        pass
+        if len(self._visitors) == 0:
+            return None
+        return max(self._visitors, key = self._visitors.count)
 
 
 class Trip:
+
+    all = []
     
     def __init__(self, visitor, national_park, start_date, end_date):
         self.visitor = visitor
@@ -24,17 +45,78 @@ class Trip:
         self.start_date = start_date
         self.end_date = end_date
 
+        self.visitor._trips.append(self)
+        self.visitor._national_parks.append(self.national_park)
+
+        self.national_park._trips.append(self)
+        self.national_park._visitors.append(self.visitor)
+
+        Trip.all.append(self)
+
+    @property 
+    def visitor(self):
+        return self._visitor 
+    
+    @visitor.setter 
+    def visitor(self, visitor):
+        if isinstance(visitor, Visitor):
+            self._visitor = visitor
+        else:
+            raise Exception("National Park must be of class Natinal Park")
+        
+    @property 
+    def start_date(self):
+        return self._start_date
+    
+    @start_date.setter 
+    def start_date(self, start_date):
+        if isinstance(start_date, str) and len(start_date) >= 7:
+            self._start_date = start_date 
+        else:
+            raise Exception("date must be longer than 6 len")
+        
+    @property 
+    def end_date(self):
+        return self._end_date
+    
+    @end_date.setter 
+    def end_date(self, end_date):
+        if isinstance(end_date, str) and len(end_date) >= 7:
+            self._end_date = end_date
+        else:
+            raise Exception("date must be longer than 6 len")
+        
+
 
 class Visitor:
 
     def __init__(self, name):
         self.name = name
+        self._trips = []
+        self._national_parks = []
+
+    @property 
+    def name(self):
+        return self.__name
+    
+    @name.setter 
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 15:
+            self._name = name 
+        else:
+            raise Exception("no")
         
     def trips(self):
-        pass
+        return self._trips
     
     def national_parks(self):
-        pass
+        return list(set(self._national_parks))
     
     def total_visits_at_park(self, park):
-        pass
+        total_visits = 0
+
+        for nat_park in self._national_parks:
+            if park == nat_park:
+                total_visits += 1
+        
+        return total_visits
