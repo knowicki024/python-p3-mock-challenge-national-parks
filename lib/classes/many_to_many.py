28b358512aa989fd171efb24f6bd9f9902e727cf
Katie N <146477745+knowicki024@users.noreply.github.com>
2024-01-18 09:19:10 -0500

    reset branch

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -2,22 +2,22 @@ class NationalPark:
 
     def __init__(self, name):
         self.name = name
-        
+
     def trips(self):
         pass
-    
+
     def visitors(self):
         pass
-    
+
     def total_visits(self):
         pass
-    
+
     def best_visitor(self):
         pass
 
 
 class Trip:
-    
+
     def __init__(self, visitor, national_park, start_date, end_date):
         self.visitor = visitor
         self.national_park = national_park
@@ -29,12 +29,12 @@ class Visitor:
 
     def __init__(self, name):
         self.name = name
-        
+
     def trips(self):
         pass
-    
+
     def national_parks(self):
         pass
-    
+
     def total_visits_at_park(self, park):
         pass
